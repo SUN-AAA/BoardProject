1665653484a500d934f6dd4d42528a3c5e211c44
SUN-AAA <seouna06@gmail.com>
2026-02-12 21:35:14 +0900

    Structure upload

---
+++ BoardProject_Backend/src/main/java/com/domain/boardproject_backend/BoardProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.domain.boardproject_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardProjectBackendApplication.class, args);
+    }
+
+}
---
+++ BoardProject_Backend/src/test/java/com/domain/boardproject_backend/BoardProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.boardproject_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
